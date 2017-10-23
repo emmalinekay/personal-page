f281fa2d984fd54ebad40446443f086442bdbb0b
Emmaline Salles <emmalinesalles@gmail.com>
2017-10-23 16:23:19 -0400

    still toying

---
+++ style.js
@@ -0,0 +1,15 @@
+$('.contact-header').hover(function () {
+  $('.contact-header').addClass('animated flipInX');
+});
+
+$('.intro-heading').hover(function () {
+  $('.intro-heading').addClass('animated flipInX');
+});
+
+$('.project-heading').hover(function () {
+  $('.project-heading').addClass('animated flipInX');
+});
+
+$('.skills-text').hover(function () {
+  $('.skills-text').addClass('animated flipInX');
+});
